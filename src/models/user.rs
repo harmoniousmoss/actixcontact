37d4d889cdb6b734af894dce419bf76bfe78f402
harmoniousmoss <syaefulbahri@protonmail.com>
2024-12-26 11:13:29 +0700

    use uuid and timestamp

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,7 +1,14 @@
-use serde::Deserialize;
+// src/models/user.rs
+use chrono::NaiveDateTime;
+use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
+use uuid::Uuid;
 
-#[derive(Deserialize)]
+#[derive(FromRow, Serialize, Deserialize)] // Derive Deserialize for Actix Web compatibility
 pub struct User {
+    pub id: Uuid,
     pub username: String,
     pub password: String,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
 }
