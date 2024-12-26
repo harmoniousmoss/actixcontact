438a0385882b257011f570be843aa2e9d023e977
harmoniousmoss <syaefulbahri@protonmail.com>
2024-12-26 11:39:53 +0700

    add route contact

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,12 +1,11 @@
-// src/routes/mod.rs
-
 use crate::handlers::{auth, contact};
 use actix_web::{web, HttpResponse};
 
 pub fn configure_routes(cfg: &mut web::ServiceConfig) {
     cfg.route("/", web::get().to(index)) // Root route
         .route("/login", web::post().to(auth::login)) // Login route
-        .route("/contact", web::get().to(contact::contact)); // Contact route
+        .route("/contact", web::post().to(contact::post_contact)) // Public POST /contact
+        .route("/contact", web::get().to(contact::get_all_contact)); // Protected GET /contact
 }
 
 // Home route handler.
