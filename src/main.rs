ef90e747b4a953d70e492e6d56e3695d005528b0
harmoniousmoss <syaefulbahri@protonmail.com>
2024-12-26 11:13:05 +0700

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,44 @@
 mod config;
 mod handlers;
+mod init;
 mod models;
+mod routes;
 mod utils;
 
-use actix_web::{web, App, HttpResponse, HttpServer};
-use sqlx::{Executor, PgPool};
-
-async fn index() -> HttpResponse {
-    HttpResponse::Ok().body("Welcome to Actix Web!")
-}
-
-async fn initialize_database(db_pool: &PgPool) {
-    let create_table_query = r#"
-    CREATE TABLE IF NOT EXISTS users (
-        id SERIAL PRIMARY KEY,
-        username VARCHAR(255) UNIQUE NOT NULL,
-        password VARCHAR(255) NOT NULL
-    );
-    "#;
-
-    db_pool
-        .execute(create_table_query)
-        .await
-        .expect("Failed to initialize database");
-}
+use actix_web::App;
+use sqlx::PgPool;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Initialize environment variables
     config::init();
 
+    // Establish database connection
     let database_url = config::get_env("DATABASE_URL");
     let db_pool = PgPool::connect(&database_url)
         .await
         .expect("Failed to connect to the database");
+    println!("✅ Connected to the database at {}", database_url);
 
-    initialize_database(&db_pool).await;
+    // Initialize database schema
+    init::initialize_database(&db_pool).await;
 
-    handlers::auth::seed_admin(&db_pool).await;
-
-    let server = HttpServer::new(move || {
-        App::new()
-            .app_data(web::Data::new(db_pool.clone()))
-            .route("/", web::get().to(index))
-            .route("/login", web::post().to(handlers::auth::login))
-            .route("/contact", web::get().to(handlers::contact::contact))
-    })
-    .bind(("127.0.0.1", 8080))?;
-
-    let addr = server.addrs();
-    for a in addr {
-        println!("Server running at http://{}", a);
+    // Seed admin user
+    match init::seed_admin(&db_pool).await {
+        Ok(message) => println!("{}", message),
+        Err(err) => println!("❌ Failed to seed admin user: {}", err),
     }
 
-    server.run().await
+    // Start HTTP server
+    let port = 8080;
+    println!("🚀 Starting server at http://127.0.0.1:{}", port);
+
+    actix_web::HttpServer::new(move || {
+        App::new()
+            .app_data(actix_web::web::Data::new(db_pool.clone()))
+            .configure(routes::configure_routes) // Centralized route configuration
+    })
+    .bind(("127.0.0.1", port))?
+    .run()
+    .await
 }
