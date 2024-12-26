c482fb9ea52d7ab3c46aa02a7026af8e1cdc86fc
harmoniousmoss <syaefulbahri@protonmail.com>
2024-12-26 11:13:14 +0700

    Refactor

--- src/handlers/auth.rs
+++ src/handlers/auth.rs
@@ -1,51 +1,50 @@
-use crate::config::get_env;
-use crate::models::user::User;
+// src/handlers/auth.rs
 use crate::utils::{hash::verify_password, jwt::generate_token};
 use actix_web::{web, HttpResponse};
+use chrono::NaiveDateTime;
+use serde::Deserialize;
 use serde_json::json;
 use sqlx::PgPool;
+use uuid::Uuid;
 
-pub async fn login(db_pool: web::Data<PgPool>, login_data: web::Json<User>) -> HttpResponse {
+// Struct for login request payload
+#[derive(Deserialize)]
+pub struct LoginRequest {
+    pub username: String,
+    pub password: String,
+}
+
+pub async fn login(
+    db_pool: web::Data<PgPool>,
+    login_data: web::Json<LoginRequest>,
+) -> HttpResponse {
     let username = &login_data.username;
     let password = &login_data.password;
 
-    let query = "SELECT password FROM users WHERE username = $1";
-    let row = sqlx::query_scalar::<_, String>(query)
+    let query = r#"
+        SELECT id, password, created_at, updated_at
+        FROM users
+        WHERE username = $1
+    "#;
+
+    let row = sqlx::query_as::<_, (Uuid, String, NaiveDateTime, NaiveDateTime)>(query)
         .bind(username)
         .fetch_optional(db_pool.get_ref())
         .await;
 
     match row {
-        Ok(Some(hashed_password)) if verify_password(&hashed_password, password) => {
+        Ok(Some((user_id, hashed_password, created_at, updated_at)))
+            if verify_password(&hashed_password, password) =>
+        {
             let token = generate_token(username);
-            HttpResponse::Ok().json(json!({ "token": token }))
+            HttpResponse::Ok().json(json!({
+                "id": user_id,
+                "username": username,
+                "token": token,
+                "created_at": created_at,
+                "updated_at": updated_at
+            }))
         }
         _ => HttpResponse::Unauthorized().json(json!({ "error": "Invalid credentials" })),
-    }
-}
-
-pub async fn seed_admin(db_pool: &PgPool) {
-    let username = get_env("ADMIN_USERNAME");
-    let password = get_env("ADMIN_PASSWORD");
-
-    let existing_user: Option<(i32,)> = sqlx::query_as("SELECT id FROM users WHERE username = $1")
-        .bind(&username)
-        .fetch_optional(db_pool)
-        .await
-        .expect("Failed to check existing admin");
-
-    if existing_user.is_none() {
-        let hashed_password = crate::utils::hash::hash_password(&password);
-
-        sqlx::query("INSERT INTO users (username, password) VALUES ($1, $2)")
-            .bind(&username)
-            .bind(&hashed_password)
-            .execute(db_pool)
-            .await
-            .expect("Failed to seed admin user");
-
-        println!("Admin user seeded: {}", username);
-    } else {
-        println!("Admin user already exists: {}", username);
     }
 }
